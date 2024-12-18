96a1285169789f5372c5e9ecd5f5c61d8a0797be
OatmealDome <julian@oatmealdome.me>
2024-12-18 16:12:22 -0500

    flake: Use wheel 0.45.1 to workaround a bug

--- flake.nix
+++ flake.nix
@@ -8,6 +8,22 @@
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
+      # Manually override wheel package to 0.45.1 until the change lands in nixos-24.11
+      # https://github.com/NixOS/nixpkgs/pull/361930
+      (self: super: rec {
+        python3 = super.python3.override {
+          packageOverrides = python-self: python-super: {
+            wheel = python-super.wheel.overridePythonAttrs (oldAttrs: rec {
+              version = "0.45.1";
+              
+              src = oldAttrs.src.override {
+                rev = "refs/tags/0.45.1";
+                hash = "sha256-tgueGEWByS5owdA5rhXGn3qh1Vtf0HGYC6+BHfrnGAs=";
+              };
+            });
+          };
+        };
+      })
       poetry2nix.overlays.default
       (final: prev: {
         central = prev.poetry2nix.mkPoetryApplication {
