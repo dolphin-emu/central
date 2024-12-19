81570307ebb3a69b6708bce909fefaf48c836143
OatmealDome <julian@oatmealdome.me>
2024-12-19 02:18:11 -0500

    4

--- flake.nix
+++ flake.nix
@@ -8,27 +8,28 @@
 
   outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
     overlay = nixpkgs.lib.composeManyExtensions [
+      # Manually override wheel package to 0.45.1 until the change lands in nixos-24.11
+      # https://github.com/NixOS/nixpkgs/pull/361930
+      (final: prev: {
+        pythonPackagesExtensions = prev.pythonPackagesExtensions ++ [
+          (python-final: python-prev: {
+            wheel = python-prev.wheel.overrideAttrs (oldAttrs: {
+              version = "0.45.1";
+              
+              src = oldAttrs.src.override {
+                rev = "refs/tags/0.45.1";
+                hash = "sha256-tgueGEWByS5owdA5rhXGn3qh1Vtf0HGYC6+BHfrnGAs=";
+              };
+            });
+          })
+        ];
+      })
       poetry2nix.overlays.default
       (final: prev: {
         central = prev.poetry2nix.mkPoetryApplication {
           projectDir = ./.;
           overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: {
             pypeul = super.pypeul.overridePythonAttrs (old: { buildInputs = (old.buildInputs or []) ++ [ super.poetry-core ]; });
-            # Manually override wheel package to 0.45.1 until the change lands in nixos-24.11
-            # https://github.com/NixOS/nixpkgs/pull/361930
-            wheel = super.wheel.overridePythonAttrs (old: {
-              version = "0.45.1";
-              src = old.src.override {
-                rev = "refs/tags/0.45.1";
-                hash = "sha256-tgueGEWByS5owdA5rhXGn3qh1Vtf0HGYC6+BHfrnGAs=";
-              };
-            });
-            setuptools = super.setuptools.overridePythonAttrs (old: {
-              version = "75.2.0";
-              src = old.src.override {
-                rev = "refs/tags/75.2.0";
-              };
-            });
           });
         };
       })
