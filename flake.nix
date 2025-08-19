a45b6c09d4f39d8d8b42080fa0a1fd0131f67b9b
OatmealDome <julian@oatmealdome.me>
2025-08-19 02:42:43 +0000

    flake: Convert to uv2nix

--- flake.nix
+++ flake.nix
@@ -3,34 +3,53 @@
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-  inputs.poetry2nix.url = "github:nix-community/poetry2nix";
-  inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = { self, nixpkgs, flake-utils, poetry2nix }: {
-    overlay = nixpkgs.lib.composeManyExtensions [
-      poetry2nix.overlays.default
-      (final: prev: {
-        central = prev.poetry2nix.mkPoetryApplication {
-          projectDir = ./.;
-          overrides = prev.poetry2nix.defaultPoetryOverrides.extend (self: super: {
-            pypeul = super.pypeul.overridePythonAttrs (old: { buildInputs = (old.buildInputs or []) ++ [ super.poetry-core ]; });
-          });
+  inputs.uv2nix.url = "github:pyproject-nix/uv2nix";
+  inputs.uv2nix.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.uv2nix.inputs.pyproject-nix.follows = "pyproject-nix";
+
+  inputs.pyproject-nix.url = "github:pyproject-nix/pyproject.nix";
+  inputs.pyproject-nix.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.pyproject-build-systems.url = "github:pyproject-nix/build-system-pkgs";
+  inputs.pyproject-build-systems.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.pyproject-build-systems.inputs.pyproject-nix.follows = "pyproject-nix";
+
+  outputs = { self, nixpkgs, flake-utils, uv2nix, pyproject-nix, pyproject-build-systems }:
+  let
+    perSystem = flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        workspace = uv2nix.lib.workspace.loadWorkspace { workspaceRoot = ./.; };
+        overlay = workspace.mkPyprojectOverlay {
+          sourcePreference = "wheel";
         };
-      })
-    ];
-  } // (flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ self.overlay ];
-      };
-    in rec {
-      packages.central = pkgs.central;
-      defaultPackage = pkgs.central;
+        python = pkgs.python310;
+        pythonSet =
+          (pkgs.callPackage pyproject-nix.build.packages { inherit python; })
+          .overrideScope (pkgs.lib.composeManyExtensions [
+            pyproject-build-systems.overlays.default
+            overlay
+          ]);
+        venv = pythonSet.mkVirtualEnv "central-env" workspace.deps.default;
+      in {
+        packages.central = venv;
+        packages.default = venv;
 
-      devShells.default = with pkgs; mkShell {
-        buildInputs = [ poetry uv ];
+        apps.default = {
+          type = "app";
+          program = "${venv}/bin/central";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.uv ];
+        };
+      });
+  in
+    perSystem // {
+      overlays.default = final: prev: {
+        central = self.packages.${final.system}.central;
       };
-    }
-  ));
+      overlay = self.overlays.default;
+    };
 }
