b764d18c3363a166084833263f274e2ac199cd03
OatmealDome <julian@oatmealdome.me>
2025-08-19 02:42:43 +0000

    flake: Update to NixOS 25.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Central, Dolphin's CI/CD plumbing infrastructure";
 
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
   inputs.poetry2nix.url = "github:nix-community/poetry2nix";
   inputs.poetry2nix.inputs.nixpkgs.follows = "nixpkgs";
 
